bd7af6c1a411b8f05af92135681b1de6b37b9ee3
Shan-Kins <shannon_makasale@hotmail.com>
2024-11-13 12:41:41 +1300

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
+import './index.css';
+import App from './App.jsx';
 
-// Create a root element and render the App component within StrictMode
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </StrictMode>,
-)
+);
